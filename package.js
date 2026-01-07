093773388c5efbb679188e39abbe0f21bca9c16a
sikini06 <benbushabu@gmail.com>
2026-01-07 16:01:57 +0100

    Add files via upload

---
+++ package.js
@@ -0,0 +1,22 @@
+﻿{
+  "name": "shorts-generator-pro",
+  "version": "1.0.0",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js",
+    "dev": "nodemon app.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "mongoose": "^7.5.0",
+    "cors": "^2.8.5",
+    "dotenv": "^16.3.1",
+    "axios": "^1.5.0",
+    "fluent-ffmpeg": "^2.1.2",
+    "youtube-dl-exec": "^2.3.1",
+    "@aws-sdk/client-s3": "^3.398.0",
+    "openai": "^4.0.0",
+    "bcryptjs": "^2.4.3",
+    "jsonwebtoken": "^9.0.2"
+  }
+}
